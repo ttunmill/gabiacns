6e2c9d312fc31aaa36cdb3bc904a71bf17d85ef5
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-11 16:34:10 +0900

    desktop

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 let sign_up = document.querySelector(".sign_up_btn")
+let nav_bg  = document.querySelector(".nav_bg")
+let sub = document.querySelectorAll(".sub")
+let family_site = document.querySelector(".family_site")
 sign_up.style.display = "none"
+nav_bg.style.display = "none"
+family_site.style.display = "none"
+for(var i of sub) {i.style.display = "none"}
+
+
+
+
+
+
+
+// swiper
+const swiper1 = new Swiper(".header_swiper", {
+    autoplay : {delay : 5000},
+    loop : true,
+})
+const swiper2 = new Swiper(".sec03_swiper", {
+    autoplay : {delay : 5000},
+    effect : "cards",
+    loop : true,
+})
