a9650766a8ee43e9e49188e107ea3c5a73b5e14e
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-11 21:36:52 +0900

    javascript

--- js/script.js
+++ js/script.js
@@ -2,11 +2,28 @@ let sign_up = document.querySelector(".sign_up_btn")
 let nav_bg  = document.querySelector(".nav_bg")
 let sub = document.querySelectorAll(".sub")
 let family_site = document.querySelector(".family_site")
+let gnb = document.querySelector(".gnb")
+let nav = document.querySelector("nav")
+let nav_clone = nav.cloneNode(true)
 sign_up.style.display = "none"
-nav_bg.style.display = "none"
 family_site.style.display = "none"
-for(var i of sub) {i.style.display = "none"}
 
+console.log(nav_clone)
+
+// navigation
+nav_bg.style.opacity = "0"
+nav_bg.style.transition = "all 0.3s"
+for(var i of sub) {i.style.opacity = "0"}
+for(var i of sub) {i.style.transition = "all 0.3s"}
+
+nav.addEventListener("mouseover", () => {
+    nav_bg.style.opacity = 1;
+    for(var i of sub) {i.style.opacity = "1"}
+})
+nav.addEventListener("mouseout", () => {
+    nav_bg.style.opacity = 0;
+    for(var i of sub) {i.style.opacity = "0"}
+})
 
 
 
