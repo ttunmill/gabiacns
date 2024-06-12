a6addd0d3927c2b0c7ee4888f54b01f9ab6876b2
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-12 12:37:57 +0900

    javascript

--- js/script.js
+++ js/script.js
@@ -5,12 +5,16 @@ let family_site = document.querySelector(".family_site")
 let gnb = document.querySelector(".gnb")
 let nav = document.querySelector("nav")
 let nav_clone = nav.cloneNode(true)
+let m_menu = document.querySelector(".m_menu")
+m_menu.appendChild(nav_clone)
+
+
+
 sign_up.style.display = "none"
 family_site.style.display = "none"
 
-console.log(nav_clone)
 
-// navigation
+// p-navigation
 nav_bg.style.height = "0"
 nav_bg.style.opacity = "0"
 nav_bg.style.transition = "opacity 0.3s"
@@ -31,18 +35,84 @@ nav.addEventListener("mouseout", () => {
     for(var i of sub) {i.style.opacity = "0"}
 })
 
+// m-navigation
+let m_gnb_li = nav_clone.querySelectorAll(".gnb > li")
+let m_gnb_sub = nav_clone.querySelectorAll(".gnb > li > .sub")
+let m_menu_close = nav_clone.querySelector(".m_menu > a > img")
+
+m_menu_close.src = "./images/close_menu.svg"
+
+nav_clone.style.transform = "translateX(-100%)"
+nav_clone.style.transition = "all 0.3s"
+m_menu.children[0].addEventListener("click", (e) => {
+    e.preventDefault();
+    nav_clone.style.left = "70%"
+})
+m_menu_close.addEventListener("click", (e) => {
+    e.preventDefault();
+    nav_clone.style.left = "0%"
+})
+for(var i of m_gnb_sub) {i.style.display = "none"}
+m_gnb_li.forEach((i, j) => {
+    i.addEventListener("click", (e) => {
+        e.preventDefault()
+        for(var i of m_gnb_sub) {i.style.display = "none"}
+        m_gnb_sub[j].style.display = "block"
+    })
+});
 
 
 
 
 
-// swiper
+// swiper plugin
 const swiper1 = new Swiper(".header_swiper", {
     autoplay : {delay : 5000},
     loop : true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next", 
+        prevEl:".swiper-button-prev"
+    },
 })
 const swiper2 = new Swiper(".sec03_swiper", {
     autoplay : {delay : 5000},
     effect : "cards",
     loop : true,
 })
+
+// scrollTrigger plugin
+// 
+// for(let i of move_up) {
+//     ScrollTrigger.create({
+//         trigger : i,
+//         onEnter : () => {i.classList.add("move_up")}
+//     })
+// }
+
+// window size responsive
+window.onload = () => {
+    let screen = window.innerWidth;
+    if(screen > 960) {
+        nav_bg.style.display = "block"
+        gnb.style.display = "flex"
+    } else {
+        nav_bg.style.display = "none"
+        gnb.style.display = "none"
+    }
+}
+
+window.onresize = function(event){
+    let innerWidth = window.innerWidth;
+    if(innerWidth > 960) {
+        nav_bg.style.display = "block"
+        gnb.style.display = "flex"
+    } else {
+        nav_bg.style.display = "none"
+        gnb.style.display = "none"
+    }
+}
+
