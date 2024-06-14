dbe2a09bbb72a00fb8ba414420673528bb262d20
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-14 17:34:33 +0900

    subpage

---
+++ pages/js/clickn.js
@@ -0,0 +1,88 @@
+// var banner_swiper = new Swiper('.banner_swiper', { 
+//     필요한 옵션을 작성합니다.
+//     autoplay: true,
+//     slidesPerView: 3,
+//     effect: 'coverflow',
+//     loop: true,
+// })
+
+const swiperSlides = document.querySelectorAll('.banner_swiper');
+
+swiperSlides.forEach(function (element, index) {
+    element.classList.add("swiper-" + index);
+    let swiper = new Swiper(".swiper-" + index, {
+    autoplay: {
+            delay: 1,
+            desableOnInteraction: false,
+    },
+    speed: 5000,
+    loop: true,
+    slidesPerView: 4,
+    freemode: true
+    });
+});
+
+
+$(function () {
+	const cvbtn = $('.scroll_btn');
+	const height = 100;
+
+	cvbtn.addClass('hide');
+
+    // スクロール
+	const scrollEv = function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > height + 400) {
+				cvbtn.removeClass('hide');
+				cvbtn.show().addClass('show');
+			} else {
+				cvbtn.removeClass('show');
+				cvbtn.addClass('hide');
+			}
+		});
+	}
+
+
+	window.addEventListener('scroll', scrollEv);
+
+	// スクロール固定
+	const cvbtnArea = $('.scroll_btn');
+
+	function checkbtn(element, check = 'above') {
+		const viewportHeight = $(window).height();
+		const scrolltop = $(window).scrollTop();
+		const elementHeight = $(element).height();
+		const y = $(element).offset().top;
+
+		if (check == "visible")
+			return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
+
+		if (check == "above")
+			return ((y < (viewportHeight + scrolltop)));
+	}
+
+	const absolEv = function () {
+		const sectionBtnl = $('.section_cvbtnsec');
+
+		if (checkbtn(sectionBtnl)) {
+			cvbtn.removeClass('fixed');
+			cvbtn.addClass('absol');
+		} else {
+			cvbtn.removeClass('absol');
+			cvbtn.addClass('fixed');
+		}
+	}
+
+	const heightEv = function () {
+		const cvbtnHeight = cvbtn.innerHeight();
+		if (window.matchMedia("(max-width: 640px)").matches) {
+			$(cvbtnArea).height(cvbtnHeight);
+		} else {
+			$(cvbtnArea).height(cvbtnHeight);
+		}
+	}
+
+	window.addEventListener('load', heightEv);
+	window.addEventListener('resize', heightEv);
+	window.addEventListener('scroll', absolEv);
+});
---
+++ pages/js/login.js
@@ -0,0 +1,5 @@
+const submit = document.querySelector(".user_btn button")
+console.log(submit)
+submit.addEventListener("click", () => {
+    location.href = "../index.html"
+})
